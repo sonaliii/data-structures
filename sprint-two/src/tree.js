3edaedd15cd66ca0d0aad5dc732a2e29bf9bc0e9
Hack Reactor <hackreactor@HR-A1.local>
2014-01-09 14:23:35 -0800

    Finished 'tree' implementation

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,31 +2,49 @@ var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
-  return _.extend(newTree, treeMethods);
+
+  return $.extend(newTree, treeMethods);;
 };
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+//set value as the target of the node
+//add that node as the child of the tree
+  var subTree = makeTree(value);
 
-  var subTree = this.makeTree(value);
-
-  if (this.children !== undefined)
-  this.children.push();
+  if (this.children === undefined) {
+    this.children = [];
+  }
+  this.children.push(subTree);
 };
 
 treeMethods.contains = function(target){
+// returns true if the target can be found as the value of the target node OR any descendants
+  var found = false;
 
+  var search = function(treeNode){
+    if (treeNode !== undefined && treeNode.value === target){
+      found = true;
+    }
+    if (treeNode.children){
+      for (var i = 0; i < treeNode.children.length; i++){
+        search(treeNode.children[i]);
+      }
+    }
+  }
+  search(this);
+  return found;
 };
 
 
 
 
 
-var theTree = makeTree(something)//create a new tree with empty '.children' array and a '.value'
+//var theTree = makeTree(something)//create a new tree with empty '.children' array and a '.value'
 
-theTree.addChild(somethingElse)//create a new tree; edit theTree's '.children' array to include this new tree
+//theTree.addChild(somethingElse)//create a new tree; edit theTree's '.children' array to include this new tree
 
 //create a new tree
 //when you create the new tree, if it's the first tree, leave children undefined
